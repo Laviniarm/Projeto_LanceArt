e29da653d8f946db6947362bb2c57d1e5939e6db
buenorafa <rafaelbuenooo@outlook.com>
2024-09-30 20:30:58 -0300

    refactor: reorganização das rotas com AuthGuard para proteger rotas de cadastro de usuário
    - Reestruturadas as rotas no `app-routing.module.ts` para aplicar o AuthGuard nas rotas de cadastro de usuário, garantindo acesso apenas para usuários autenticados.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,23 @@ import { CadastroComponent } from './auth/cadastro/cadastro.component';
 import { CadastroObrasComponent } from './obras-de-arte/cadastro-obras/cadastro-obras.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { HomeComponent } from './shared/components/home/home.component';
+import { ListagemObrasComponent } from './obras-de-arte/listagem-obras/listagem-obras.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/cadastro', component: CadastroComponent },
+  {
+    path: 'listagemArte',
+    component: ListagemObrasComponent,
+    canActivate: [authGuard],
+  },
   {
     path: 'cadastrarArte',
     component: CadastroObrasComponent,
     canActivate: [authGuard],
   },
-  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
